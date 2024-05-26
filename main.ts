1216aa1fc360a65ff9204a6a2c7ab189580ac0f8
dnag220882 <158020112+dnag220882@users.noreply.github.com>
2024-05-26 14:28:34 -0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,9 @@
+input.onButtonPressed(Button.A, function () {
+    record.startRecording(record.BlockingState.Blocking)
+})
+input.onButtonPressed(Button.B, function () {
+    record.playAudio(record.BlockingState.Blocking)
+})
+basic.forever(function () {
+	
+})
